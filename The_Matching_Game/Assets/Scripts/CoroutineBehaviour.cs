b09817c387ee970e9434fc9359cfa2401725a729
Kyra Ellen <10883023@uvu.edu>
2022-09-21 17:17:40 -0600

    coroutines

---
+++ The_Matching_Game/Assets/Scripts/CoroutineBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+    public UnityEvent repeatEvent;
+    
+    public int counterNum = 3;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+    private IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        Debug.Log("Start");
+        yield return wfsObj;
+        Debug.Log("Late Start");
+
+        while (counterNum > 0)
+        {
+            yield return wfsObj;
+            repeatEvent.Invoke();
+        }
+    }
+}
